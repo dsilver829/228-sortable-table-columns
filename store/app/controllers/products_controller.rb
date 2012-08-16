5e015b778850d5886b257c93238b8e697a01ba84
David Silver <dsilver@candidatemetrics.com>
2012-08-16 14:41:59 -0700

    sorting works

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   helper_method :sort_column, :sort_direction
   
   def index
-    @products = Product.order(sort_column + " " + sort_direction)
+    @products = Product.order(params[:sort])
   end
   
   def show
